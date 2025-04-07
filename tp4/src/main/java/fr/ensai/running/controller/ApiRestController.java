ae3b952ec739995954198aa30ccfa2b808c8af6a
Pascale29 <pascale-mariane.alapini@eleve.ensai.fr>
2025-04-07 13:20:07 +0000

    Ajout des endpoints

--- tp4/src/main/java/fr/ensai/running/controller/ApiRestController.java
+++ tp4/src/main/java/fr/ensai/running/controller/ApiRestController.java
@@ -3,7 +3,13 @@ package fr.ensai.running.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,6 +30,41 @@ public class ApiRestController {
     public List<Athlete> allAthletes() {
 
         return athleteService.findAll();
+    }
+
+    /**
+     * Get an athlete by id
+     */
+    @GetMapping("/athlete/{id}")
+    public ResponseEntity<Athlete> getAthleteById(@PathVariable Long id) {
+        Athlete athlete = athleteService.findById(id);
+        if (athlete == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(athlete);
+    }
+
+    /**
+     * Delete an athlete by id
+     */
+    @DeleteMapping("/athlete/{id}")
+    public ResponseEntity<Void> deleteAthleteById(@PathVariable Long id) {
+        Athlete athlete = athleteService.findById(id);
+        if (athlete == null) {
+            return ResponseEntity.notFound().build();
+        }
+        athleteService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+    
+
+    /**
+     * Create an athlete
+     */
+    @PostMapping("/athlete")
+    public ResponseEntity<Athlete> createAthlete(@RequestBody Athlete athlete) {
+        Athlete createdAthlete = athleteService.save(athlete);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdAthlete);
     }
 
 }
